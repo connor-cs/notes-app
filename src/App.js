5b7944f41b7e3c6987d05e71e074517e4bc54c51
Connor <connor.cs@protonmail.com>
2023-01-01 14:04:04 -0500

    notes and noteslist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+export default function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "sneed's feed and seed",
+      date: "1/1/2023",
+    },
+    {
+      id: nanoid(),
+      text: "formerly chuck's",
+      date: "1/1/2023",
+    },
+    {
+      id: nanoid(),
+      text: "sergey betray",
+      date: "1/1/2023",
+    },
+    {
+      id: nanoid(),
+      text: "snack city",
+      date: "1/1/2023",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NotesList notes={notes} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Note from "./Note";
+
+export default function NotesList({ notes }) {
+  return (
+    <div className="notes-list">
+      {notes.map((note) => {
+        return <Note id={note.id} text={note.text} date={note.date} />;
+      })}
+    </div>
+  );
+}
