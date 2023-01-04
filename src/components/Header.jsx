597748e1197b7cb555f0530c3bbf3fd492d1035c
Connor <connor.cs@protonmail.com>
2023-01-04 14:05:29 -0500

    final branch commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,12 @@ export default function Header({ setDarkMode }) {
   return (
     <div className="header">
       <h1>Notes</h1>
-      <CFormSwitch label="Toggle dark mode" id="formSwitchCheckDefault" />
+      <CFormSwitch
+        label="Toggle dark mode"
+        id="formSwitchCheckDefault"
+        onChange={(darkMode) => setDarkMode(!darkMode)}
+        // onChange={() => console.log('abc')}
+      />
     </div>
   );
 }
